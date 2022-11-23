e37b98b0e9dd7f06edb22ff7522b9e489c35a505
trantienvip <trantienvip>
2022-11-24 01:46:32 +0700

    update home head

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
-console.log('Okse');
+$("#search-input").keyup(function () {
+    clearSearch();
+});
+
+$('.menu-search .close').click(function () {
+    $('#search-input').val('');
+    clearSearch();
+})
+
+function clearSearch() {
+    if ($('#search-input').val().length > 0) {
+        $('.menu-search .close').addClass('active');
+    } else {
+        $('.menu-search .close').removeClass('active');
+    }
+}
+
+$('.search.icon-search').click(function () {
+    $('.menu-search').toggleClass('active');
+    $('.full-page').toggleClass('active');
+})
+
+$('.full-page').click(function () {
+    $('.menu-search').removeClass('active');
+    $('.full-page').removeClass('active');
+})
