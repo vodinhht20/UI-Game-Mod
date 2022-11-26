0846bd24b8fc6bb3ffb0d0210df864cc24df4cec
Võ Định <vodinh2000ht@gmail.com>
2022-11-26 10:42:38 +0700

    fix Ui

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,3 +33,41 @@ $('.click-tog').click(function () {
 $('.click-tog2').click(function () {
     $('.tog2').toggleClass('active');
 })
+
+
+$('.box-collapse .nav-dropdown li').click(function (e) {
+    let tabName = e.target.getAttribute('data-name');
+    $('.wrapper-game-app-page #wrapper-tabs .tab-pane').removeClass(['active', 'show']);
+    $(`#${tabName}`).addClass(['active', 'show']);
+    putParam('tab-game', tabName);
+    activedItemByParameter('tab-game', '.wrapper-game-app-page .box-collapse .nav-dropdown li', 'data-name', ['active']);
+})
+
+function activedItemByParameter(name, className, dataElementName, addClass = ['active', 'show']) {
+    let currentTab = getParameter(name);
+    if (!currentTab) return;
+
+    let addedClass = '';
+    $(className).removeClass(addClass);
+    $(className).addClass(function(index) {
+        let dataElementValue = document.querySelectorAll(className)[index].getAttribute(dataElementName);
+        if (dataElementValue == currentTab) {
+            return addClass.join(' ');
+        }
+        return addedClass;
+    });
+}
+
+activedItemByParameter('tab-game', '.wrapper-game-app-page #wrapper-tabs .tab-pane', 'id');
+activedItemByParameter('tab-game', '.wrapper-game-app-page .box-collapse .nav-dropdown li', 'data-name', ['active']);
+
+function getParameter(keyParam) {
+    var params = (new URL(document.location)).searchParams;
+    return params.get(keyParam);
+}
+
+function putParam(key, value) {
+    var urlParam = new URL(window.location);
+    urlParam.searchParams.set(key, value);
+    window.history.pushState({}, '', urlParam);
+}
