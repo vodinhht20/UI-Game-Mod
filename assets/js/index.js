b16abc202c1359f21452037daeef77cd66726dfd
Võ Định <vodinh2000ht@gmail.com>
2022-11-26 11:01:11 +0700

    fix js

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,7 +34,6 @@ $('.click-tog2').click(function () {
     $('.tog2').toggleClass('active');
 })
 
-
 $('.box-collapse .nav-dropdown li').click(function (e) {
     let tabName = e.target.getAttribute('data-name');
     $('.wrapper-game-app-page #wrapper-tabs .tab-pane').removeClass(['active', 'show']);
@@ -56,6 +55,8 @@ function activedItemByParameter(name, className, dataElementName, addClass = ['a
         }
         return addedClass;
     });
+
+    activeParentByItem('#header-tab-nav .nav-item', 'li');
 }
 
 activedItemByParameter('tab-game', '.wrapper-game-app-page #wrapper-tabs .tab-pane', 'id');
@@ -70,4 +71,16 @@ function putParam(key, value) {
     var urlParam = new URL(window.location);
     urlParam.searchParams.set(key, value);
     window.history.pushState({}, '', urlParam);
+}
+
+
+function activeParentByItem(parentClass, childrenClass) {
+    $(parentClass).removeClass('active');
+    $(parentClass).addClass(function(index) {
+        let hasChidrenActive = document.querySelectorAll(parentClass)[index].querySelector(`${childrenClass}.active`);
+        if (hasChidrenActive ) {
+            return 'active';
+        }
+        return '';
+    });
 }
